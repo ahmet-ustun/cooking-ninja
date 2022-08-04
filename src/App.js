2cfdc5c4c1a38e2e4eb9e760a4701917bec3e306
Ahmet Ustun <austu@softserveinc.com>
2022-08-04 08:49:52 +0300

    setting up the routes to the pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home.js";
+import Create from "./pages/create/Create.js";
+import Search from "./pages/search/Search.js";
+import Recipe from "./pages/recipe/Recipe.js";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipe/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

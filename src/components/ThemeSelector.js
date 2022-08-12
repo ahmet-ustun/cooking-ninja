c032e39afbd4312216964198d63011d5f4230b92
Ahmet Ustun <austu@softserveinc.com>
2022-08-12 10:51:42 +0300

    adding a new logic in order to change mode inbetween light and dark

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,14 +1,27 @@
 import { useTheme } from "../hooks/useTheme.js";
 
 import "./ThemeSelector.css";
+import modeIcon from "../assets/mode-icon.svg";
 
 const themeColors = ["#58249c", "#249c6b", "#b70233"];
 
 const ThemeSelector = () => {
-  const { changeColor } = useTheme();
+  const { mode, changeColor, changeMode } = useTheme();
+
+  const toggleMode = () => {
+    changeMode(mode === "light" ? "dark" : "light");
+  };
 
   return (
     <div className="theme-selector">
+      <div className="mode-toggle">
+        <img
+          src={modeIcon}
+          alt="Light/Dark Mode Toggle Icon"
+          style={{ filter: mode === "light" ? "invert(20%)" : "invert(100%)" }}
+          onClick={toggleMode}
+        />
+      </div>
       <div className="theme-buttons">
         {themeColors.map((color) => (
           <div

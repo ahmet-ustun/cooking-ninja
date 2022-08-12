5e1e2548012f8e9e5a0b9ab6de658141291058e8
Ahmet Ustun <austu@softserveinc.com>
2022-08-12 09:14:34 +0300

    creating a custom useTheme hook for reducing boilerplate code

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+
+import { ThemeContext } from "../context/ThemeContext.js";
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+    throw new Error("useTheme() must be in a ThemeProvider");
+  }
+
+  return context;
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { ThemeProvider } from "./context/ThemeContext.js";
+
 import "./index.css";
 
 import App from "./App";
 
-import { ThemeProvider } from "./context/ThemeContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
